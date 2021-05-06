2fcc30f7cd460c598854e3ad70920de7e51c05b3
Travis CI User <travis@example.org>
2021-05-06 12:24:42 +0000

    Remove obsolete using of HOC and use hooks instead (#1246)

--- js/739.807f5a0e1d4cee28d61f.js
+++ js/739.807f5a0e1d4cee28d61f.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[739],{7739:(n,e,t)=>{var s=t(89465),o=t(55189),r=Object.prototype.hasOwnProperty,p=o((function(n,e,t){r.call(n,t)?n[t].push(e):s(n,t,[e])}));n.exports=p}}]);
-//# sourceMappingURL=../sourcemaps/739.542e3da57d43304449ec.js.map
+//# sourceMappingURL=../sourcemaps/739.91a34913b55bd10cb1a0.js.map
