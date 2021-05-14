d83518c037baeaef77ffda135099565ba2e8b1a2
Travis CI User <travis@example.org>
2021-05-14 10:53:56 +0000

    Migrate general info tab to federated modules (#1266)

--- js/665.77420d6776ad3ce59b80.js
+++ js/665.77420d6776ad3ce59b80.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[665],{14665:(n,t,e)=>{"use strict";function r(n,t){return(r=Object.setPrototypeOf||function(n,t){return n.__proto__=t,n})(n,t)}e.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.4b3d09f676036186ff89.js.map
+//# sourceMappingURL=../sourcemaps/665.a6fcefbe43525f2aef63.js.map
