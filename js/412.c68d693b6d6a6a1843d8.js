0a965a09c6d120ad4aa606fae2ed246c5c95af73
Travis CI User <travis@example.org>
2021-06-11 23:01:50 +0000

    add nightly build script (#105)

--- js/412.c68d693b6d6a6a1843d8.js
+++ js/412.c68d693b6d6a6a1843d8.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>f});var r=n(92950),o=n.n(r),s=n(45697),a=n.n(s),u=n(16530),i=n(557),l=n(30319),c=n(48187),g=n(35240),d=(n(94500),function(){var e=(0,l.S1)();return o().createElement(l.gE.Provider,{value:{getRegistry:function(){return e}}},o().createElement(i.Provider,{store:e.getStore()},o().createElement(u.BrowserRouter,{basename:(0,g.eb)(window.location.pathname)},o().createElement(c.Z,null))))});d.propTypes={useLogger:a().bool},d.defaultProps={useLogger:!1};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/412.e5106455c034a9adb25d.js.map
+//# sourceMappingURL=../sourcemaps/412.079c0049c05f7d2e7d9d.js.map
--- js/665.77420d6776ad3ce59b80.js
+++ js/665.77420d6776ad3ce59b80.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[665],{14665:(n,t,e)=>{"use strict";function r(n,t){return(r=Object.setPrototypeOf||function(n,t){return n.__proto__=t,n})(n,t)}e.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.e5106455c034a9adb25d.js.map
+//# sourceMappingURL=../sourcemaps/665.079c0049c05f7d2e7d9d.js.map
