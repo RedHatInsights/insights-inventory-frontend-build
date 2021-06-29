b655655f5232c63a66ccf5650cc3b929d2f8f923
Travis CI User <travis@example.org>
2021-06-29 07:36:06 +0000

    extend OUIA IDs in delete/edit modals (#1324)
    * extend OUIA IDs in delete/edit modals
    
    * Fixed linting issues
    
    * Update src/components/inventory/GeneralInfo/TextInputModal/TextInputModal.js
    
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- js/665.77420d6776ad3ce59b80.js
+++ js/665.77420d6776ad3ce59b80.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[665],{14665:(n,t,e)=>{"use strict";function r(n,t){return(r=Object.setPrototypeOf||function(n,t){return n.__proto__=t,n})(n,t)}e.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.3498e31995637ade9689.js.map
+//# sourceMappingURL=../sourcemaps/665.1d96a7ca1370e5920a8d.js.map
