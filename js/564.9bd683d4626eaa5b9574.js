0568ae1f5e0206ab73644eaf7b8be8548b377f5c
Travis CI User <travis@example.org>
2021-07-21 12:56:08 +0000

    Merge pull request #1336 from rvsia/updateProdBeta
    Update prod beta

--- js/564.9bd683d4626eaa5b9574.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[564],{85564:(n,e,t)=>{var r=t(21078);n.exports=function(n){return null!=n&&n.length?r(n,1):[]}}}]);
---
+++ js/8446.3d4c8525eed91f1e7293.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[8446],{18446:(n,e,t)=>{var r=t(90939);n.exports=function(n,e){return r(n,e)}}}]);
