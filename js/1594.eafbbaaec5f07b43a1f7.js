5af7ca72b5a07a14c93f348d71306f5b20bf36d5
Travis CI User <travis@example.org>
2021-07-21 14:26:19 +0000

    Merge pull request #1342 from RedHatInsights/master
    Fix gen info system detail path (#1340)

--- js/1594.eafbbaaec5f07b43a1f7.js
+++ js/1594.eafbbaaec5f07b43a1f7.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[1594],{71594:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>s});var r=t(22122),i=t(92950),a=t.n(i),f=t(47401),o=function(e){return a().createElement(f.Z,(0,r.Z)({componentName:"DetailWrapper"},e))};const s=a().forwardRef((function(e,n){return a().createElement(o,(0,r.Z)({innerRef:n},e))}))}}]);
-//# sourceMappingURL=../sourcemaps/1594.9f3cb9ec277317fe3ba5.js.map
+//# sourceMappingURL=../sourcemaps/1594.f16904518837fa92b7ce.js.map
