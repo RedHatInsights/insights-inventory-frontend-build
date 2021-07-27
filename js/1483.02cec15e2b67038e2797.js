0a30a1b617df08021ab861c89399ea1c30b4a8ea
Travis CI User <travis@example.org>
2021-07-27 10:16:06 +0000

    Merge pull request #1347 from Hyperkid123/fix-node-16
    Update webpack dependencies

--- js/1483.02cec15e2b67038e2797.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[1483],{47468:(e,n,t)=>{"use strict";t.d(n,{Z:()=>c});var s=t(92950),i=t.n(s),r=t(52643),l=t(2372);const c=i().createElement(r.b,null,i().createElement(l.$,{size:"xl"}))}}]);
-//# sourceMappingURL=../sourcemaps/1483.8538e33fbb9044f3eb2b.js.map
