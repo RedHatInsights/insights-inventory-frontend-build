bcad5ab31abe2ad849d8ca6807a730b7dd111935
Travis CI User <travis@example.org>
2021-08-03 11:28:23 +0000

    Updated delete modal cancel OUIA id and added confirm OUIA id. (#1351)
    * Updated delete modal cancel OUIA id and added confirm OUIA id.
    
    * fixed linting issues, updated snapshots

--- js/483.f355d3e969e4fb9b0a5c.js
+++ js/483.f355d3e969e4fb9b0a5c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[483],{47468:(e,n,t)=>{"use strict";t.d(n,{Z:()=>c});var s=t(92950),i=t.n(s),r=t(52643),l=t(2372);const c=i().createElement(r.b,null,i().createElement(l.$,{size:"xl"}))}}]);
-//# sourceMappingURL=../sourcemaps/483.d7c002c83d2cebfc8845.js.map
+//# sourceMappingURL=../sourcemaps/483.267d371c908642b0f898.js.map
