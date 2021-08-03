bb0b1b946ff889dffe215f26d321b80c977e1f90
Travis CI User <travis@example.org>
2021-08-03 15:44:33 +0000

    Show ROS tab only on beta environments (#1354)
    * Show ROS tab only on beta environments
    
    * Show ros tab in all envs except prod-stable

--- js/201.bf0425b0e84df180b651.js
+++ js/201.bf0425b0e84df180b651.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[201],{62201:(n,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>a});var r=t(22122),o=t(92950),f=t.n(o),i=t(47401),s=function(n){return f().createElement(i.Z,(0,r.Z)({componentName:"InventoryTable"},n))};const a=f().forwardRef((function(n,e){return f().createElement(s,(0,r.Z)({innerRef:e},n))}))}}]);
-//# sourceMappingURL=../sourcemaps/201.267d371c908642b0f898.js.map
+//# sourceMappingURL=../sourcemaps/201.3ebecc05e858f886ae06.js.map
