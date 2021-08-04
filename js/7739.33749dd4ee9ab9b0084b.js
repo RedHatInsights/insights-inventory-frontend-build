600cd55ab099b288e74e8d7f2b80ac3ed5a76149
Travis CI User <travis@example.org>
2021-08-04 10:33:48 +0000

    Merge pull request #1355 from RedHatInsights/master
    Update prod-beta to newest version

--- js/7739.33749dd4ee9ab9b0084b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[7739],{7739:(n,e,t)=>{var s=t(89465),o=t(55189),r=Object.prototype.hasOwnProperty,p=o((function(n,e,t){r.call(n,t)?n[t].push(e):s(n,t,[e])}));n.exports=p}}]);
