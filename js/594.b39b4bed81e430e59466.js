9b69bd93ea900ef2c20114b08557faa36f856c46
Travis CI User <travis@example.org>
2021-08-17 15:07:11 +0000

    Merge pull request #1368 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-notifications-3.2.4
    Bump @redhat-cloud-services/frontend-components-notifications from 3.2.2 to 3.2.4

--- js/594.b39b4bed81e430e59466.js
+++ js/594.b39b4bed81e430e59466.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[594],{71594:(e,n,t)=>{t.r(n),t.d(n,{default:()=>s});var r=t(87462),i=t(92950),a=t.n(i),f=t(47401),o=function(e){return a().createElement(f.Z,(0,r.Z)({componentName:"DetailWrapper"},e))};const s=a().forwardRef((function(e,n){return a().createElement(o,(0,r.Z)({innerRef:n},e))}))}}]);
-//# sourceMappingURL=../sourcemaps/594.707c9b3dedb0eab42297.js.map
+//# sourceMappingURL=../sourcemaps/594.443e42a5b83100c2d505.js.map
