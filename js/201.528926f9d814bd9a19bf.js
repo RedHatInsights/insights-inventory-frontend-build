1476bd460f2aa66e8972da21df2ea62772e5844a
Travis CI User <travis@example.org>
2021-08-24 16:13:55 +0000

    Use fed module from compliance instead of NPM (#1362)

--- js/201.528926f9d814bd9a19bf.js
+++ js/201.528926f9d814bd9a19bf.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[201],{62201:(n,e,t)=>{t.r(e),t.d(e,{default:()=>a});var r=t(87462),o=t(92950),f=t.n(o),i=t(47401),s=function(n){return f().createElement(i.Z,(0,r.Z)({componentName:"InventoryTable"},n))};const a=f().forwardRef((function(n,e){return f().createElement(s,(0,r.Z)({innerRef:e},n))}))}}]);
-//# sourceMappingURL=../sourcemaps/201.010940b4f618c67bd3ce.js.map
+//# sourceMappingURL=../sourcemaps/201.3c4e5fc61373fb72c87c.js.map
--- js/680.bb06bc734ba10569f33a.js
+++ js/680.bb06bc734ba10569f33a.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[680],{79680:(e,n,t)=>{t.r(n),t.d(n,{default:()=>s});var r=t(87462),i=t(92950),o=t.n(i),a=t(47401),f=function(e){return o().createElement(a.Z,(0,r.Z)({componentName:"InventoryDetailHead"},e))};const s=o().forwardRef((function(e,n){return o().createElement(f,(0,r.Z)({innerRef:n},e))}))}}]);
-//# sourceMappingURL=../sourcemaps/680.010940b4f618c67bd3ce.js.map
+//# sourceMappingURL=../sourcemaps/680.3c4e5fc61373fb72c87c.js.map
