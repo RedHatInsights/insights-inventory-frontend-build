9dca3c84b6a607fcb7398cc2fa5f552149e1a17a
Travis CI User <travis@example.org>
2021-08-25 07:18:49 +0000

    Expose all general info modules trough fed modules (#1350)

--- js/412.aa2119c30e1398b9f48a.js
+++ js/412.aa2119c30e1398b9f48a.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>f});var r=n(92950),o=n.n(r),s=n(45697),a=n.n(s),u=n(16530),i=n(45237),l=n(30319),c=n(48187),g=n(35240),d=(n(94500),function(){var e=(0,l.S1)();return o().createElement(l.gE.Provider,{value:{getRegistry:function(){return e}}},o().createElement(i.Provider,{store:e.getStore()},o().createElement(u.BrowserRouter,{basename:(0,g.eb)(window.location.pathname)},o().createElement(c.Z,null))))});d.propTypes={useLogger:a().bool},d.defaultProps={useLogger:!1};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/412.3c4e5fc61373fb72c87c.js.map
+//# sourceMappingURL=../sourcemaps/412.46deeef8a086bd0cdf53.js.map
