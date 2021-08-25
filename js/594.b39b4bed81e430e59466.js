f121718cb6afe06c484cb52d6964b79788f93b0f
Travis CI User <travis@example.org>
2021-08-25 13:33:16 +0000

    Rename system's display name on succes directly without refresh (#1378)

--- js/594.b39b4bed81e430e59466.js
+++ js/594.b39b4bed81e430e59466.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[594],{71594:(e,n,t)=>{t.r(n),t.d(n,{default:()=>s});var r=t(87462),i=t(92950),a=t.n(i),f=t(47401),o=function(e){return a().createElement(f.Z,(0,r.Z)({componentName:"DetailWrapper"},e))};const s=a().forwardRef((function(e,n){return a().createElement(o,(0,r.Z)({innerRef:n},e))}))}}]);
-//# sourceMappingURL=../sourcemaps/594.a8e32cf64fbc76cc82bd.js.map
+//# sourceMappingURL=../sourcemaps/594.c458c21ca41977c9cd36.js.map
