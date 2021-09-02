cfd21286cfdafd5e9f2aebc9c2d1e8a5eb1d4025
Travis CI User <travis@example.org>
2021-09-02 09:04:00 +0000

    Merge pull request #1386 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-config-4.3.5
    Bump @redhat-cloud-services/frontend-components-config from 4.3.3 to 4.3.5

--- js/680.bb06bc734ba10569f33a.js
+++ js/680.bb06bc734ba10569f33a.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[680],{79680:(e,n,t)=>{t.r(n),t.d(n,{default:()=>s});var r=t(87462),i=t(92950),o=t.n(i),a=t(47401),f=function(e){return o().createElement(a.Z,(0,r.Z)({componentName:"InventoryDetailHead"},e))};const s=o().forwardRef((function(e,n){return o().createElement(f,(0,r.Z)({innerRef:n},e))}))}}]);
-//# sourceMappingURL=../sourcemaps/680.c458c21ca41977c9cd36.js.map
+//# sourceMappingURL=../sourcemaps/680.ac13163b0ea12c6267b9999d49c0f888.js.map
