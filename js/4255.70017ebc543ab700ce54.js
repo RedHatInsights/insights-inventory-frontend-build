1d79b97e821df98380490cea1f097b897c00c899
Travis CI User <travis@example.org>
2022-03-28 15:27:16 +0000

    Merge pull request #1525 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-config-utilities-1.5.17
    Bump @redhat-cloud-services/frontend-components-config-utilities from 1.5.4 to 1.5.17

--- js/4255.70017ebc543ab700ce54.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[4255],{53336:()=>{},53519:()=>{}}]);
--- js/6246.5a601b940360f88843b0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[6246],{54994:()=>{},37494:()=>{},67761:()=>{}}]);
