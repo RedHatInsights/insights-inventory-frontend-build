74e1a084abe1743a688a73dedce9d9ea626b5634
Travis CI User <travis@example.org>
2022-10-25 09:46:14 +0000

    chore(deps): Bump @redhat-cloud-services & @patternfly packages (#1672)
    * chore(deps): Bump @patternfly packages
    
    * chore(deps): Bump @redhat-cloud-services packages
    
    * chore(deps): Run npm audit fix
    
    * chore(deps): Update @redhat-cloud-services packages

--- js/497.1666612219523.cd83556444edac656de6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[497],{10497:()=>{}}]);
--- js/8361.1666612219523.cd83556444edac656de6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_inventory_frontend=self.webpackChunkinsights_inventory_frontend||[]).push([[8361],{88361:()=>{}}]);
